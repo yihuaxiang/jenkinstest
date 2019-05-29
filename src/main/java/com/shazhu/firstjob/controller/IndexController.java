32e790883f7390fe5ef390b35f9609df1f4ebb56
敖武 <dongdong.fdd@alibaba-inc.com>
2019-05-30 01:06:56 +0800

    welcome

--- src/main/java/com/shazhu/firstjob/controller/IndexController.java
+++ src/main/java/com/shazhu/firstjob/controller/IndexController.java
@@ -14,4 +14,9 @@ public class IndexController {
     public String index(){
         return "hello world";
     }
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
 }
