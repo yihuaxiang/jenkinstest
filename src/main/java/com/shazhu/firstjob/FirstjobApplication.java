828698164c0a3056faebd23cb5a73a6d29ae8ad6
敖武 <dongdong.fdd@alibaba-inc.com>
2019-05-29 22:55:52 +0800

    init spring boot

---
+++ src/main/java/com/shazhu/firstjob/FirstjobApplication.java
@@ -0,0 +1,13 @@
+package com.shazhu.firstjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstjobApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/shazhu/firstjob/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.shazhu.firstjob.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dongdong.fdd
+ * @date 2019-05-29 22:52
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello world";
+    }
+}
