44841046575e682446f0675610f4edc8d7f1fb3a
敖武 <dongdong.fdd@alibaba-inc.com>
2019-09-29 10:24:42 +0800

    start.sh

---
+++ buildAndRestart.sh
@@ -0,0 +1,11 @@
+
+pid=`ps axu | grep java | grep SNAPSHOT | awk \'{print $2}\'`
+echo "pid is $pid"
+if [[ ! -z $pid ]]
+    then
+    kill -9 $pid
+fi
+
+mvn clean package
+
+nohup java -jar target/firstjob-0.0.1-SNAPSHOT.jar 2&>1 &
